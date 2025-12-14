c3da73e722ac8235f11c75f74649e03d3bcad6f3
tar455 <tariqueansari455@gmail.com>
2025-12-14 15:23:56 +0530

    added some new tab

--- backend-javaFx/react-pravaha-ui/src/component/NewTab.jsx
+++ backend-javaFx/react-pravaha-ui/src/component/NewTab.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 export default function NewTab() {
     const openNewTab = () => {
-        window.open('', '_blank');
+        window.open(window.location.href, '_blank', 'width=800,height=600');
+    };
+
+    const closeTab = () => {
+        window.close();
     };
 
     return (
-        <div>
+        <div style={{ position: 'fixed', top: 0, right: 0, display: 'flex', justifyContent: 'flex-end', zIndex: 1000 }}>
             <button onClick={openNewTab}>
-                Open New Tab
+                +
+            </button>
+            <button onClick={closeTab}>
+                x
             </button>
         </div>
     );
--- backend-javaFx/src/main/java/ai/pravaha/ui/App.java
+++ backend-javaFx/src/main/java/ai/pravaha/ui/App.java
@@ -2,20 +2,29 @@ package ai.pravaha.ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        stage.getIcons().add(
+                new Image(Objects.requireNonNull(getClass().getResourceAsStream("/image/pravahLogo.png")))
+        );
         BrowserController controller = new BrowserController();
 
         BorderPane root = controller.getUI();
         Scene scene = new Scene(root, 1200, 800);
-
+        stage.setFullScreenExitHint(""); // hides "Press ESC to exit"
+        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         stage.setScene(scene);
         stage.setTitle("pravaha");
         stage.show();
+//        stage.setFullScreen(true);
     }
 
 }
